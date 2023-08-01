2d1cae45f8af5b0a5500277508848f5e95f5140e
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-08-01 19:37:35 +1000

    init commit

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -4,15 +4,28 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int parts = scanner.nextInt();
-        int cuts;
-        int remains = 1;
+        int cuts = 0;
 
+        if (parts > 1) cuts = getCuts(parts, cuts);
+
+        System.out.println(cuts);
+    }
+
+    private static int getCuts(int parts, int cuts) {
         if (parts % 2 != 0) parts++;
 
         while (parts % 2 != 1) {
+            if (parts % 2 == 0) {
+                parts = parts / 2;
+                cuts++;
+            }
+
+            if (parts % 3 == 0) {
+                parts = parts / 3;
+                cuts= cuts + 2;
+            }
 
         }
-
-        System.out.println(9%1);
+        return cuts;
     }
 }
